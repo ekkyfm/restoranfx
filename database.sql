62fa18be489bc936931cf0c4f2efe087d3364041
Ekky Fauzan Maulana <kerja.ekky.fauzan@gmail.com>
2016-01-12 20:55:32 +0700

    push database

--- database.sql
+++ database.sql
@@ -30,9 +30,9 @@ CREATE TABLE `detail_pesanan` (
   PRIMARY KEY (`id_detail_pesanan`),
   KEY `id_pesanan` (`id_pesanan`),
   KEY `id_menu` (`id_menu`),
-  CONSTRAINT `detail_pesanan_ibfk_1` FOREIGN KEY (`id_pesanan`) REFERENCES `pesanan` (`id_pesanan`),
+  CONSTRAINT `detail_pesanan_ibfk_1` FOREIGN KEY (`id_pesanan`) REFERENCES `pesanan` (`id_pesanan`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `detail_pesanan_ibfk_2` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,9 +41,28 @@ CREATE TABLE `detail_pesanan` (
 
 LOCK TABLES `detail_pesanan` WRITE;
 /*!40000 ALTER TABLE `detail_pesanan` DISABLE KEYS */;
-INSERT INTO `detail_pesanan` VALUES (1,1,1,10),(2,2,2,2),(3,3,1,1),(4,3,5,1),(5,3,3,1),(6,0,2,1),(7,0,3,1);
+INSERT INTO `detail_pesanan` VALUES (54,17,1,2),(57,19,3,1);
 /*!40000 ALTER TABLE `detail_pesanan` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 trigger after_insert_detail_pesanan  AFTER INSERT on detail_pesanan
+for each row
+begin
+	update menu set stok = menu.stok - new.jumlah where menu.id_menu = new.id_menu;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `meja`
@@ -59,7 +78,7 @@ CREATE TABLE `meja` (
   `status` int(11) NOT NULL,
   `kapasitas` int(11) NOT NULL,
   PRIMARY KEY (`no_meja`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +87,7 @@ CREATE TABLE `meja` (
 
 LOCK TABLES `meja` WRITE;
 /*!40000 ALTER TABLE `meja` DISABLE KEYS */;
-INSERT INTO `meja` VALUES (1,'No Smoking',2,1,20);
+INSERT INTO `meja` VALUES (1,'No Smoking',2,0,20),(2,'Smoking',1,0,20),(3,'Smoking',1,0,20),(4,'Smoking',1,0,20),(5,'Smoking',1,0,20),(6,'No Smoking',2,0,77),(7,'No Smoking',3,0,2);
 /*!40000 ALTER TABLE `meja` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,13 +99,13 @@ DROP TABLE IF EXISTS `menu`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `menu` (
-  `id_menu` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL AUTO_INCREMENT,
   `nama_menu` varchar(60) NOT NULL,
   `jenis` enum('makanan','minuman') NOT NULL,
   `stok` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
   PRIMARY KEY (`id_menu`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,7 +114,7 @@ CREATE TABLE `menu` (
 
 LOCK TABLES `menu` WRITE;
 /*!40000 ALTER TABLE `menu` DISABLE KEYS */;
-INSERT INTO `menu` VALUES (1,'Nasi Goreng Kambing','makanan',10,10000),(2,'Baso','makanan',10,70000),(3,'Jus Melon','minuman',20,5000),(4,'Teh Manis','minuman',30,1),(5,'A','makanan',1,2);
+INSERT INTO `menu` VALUES (1,'Nasi Goreng Kambing','makanan',-4,10000),(2,'Baso','makanan',-1,70000),(3,'Jus Melon','minuman',-3,5000),(4,'Teh Manis','minuman',24,1),(6,'qew','makanan',0,100000);
 /*!40000 ALTER TABLE `menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,12 +126,12 @@ DROP TABLE IF EXISTS `pesanan`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pesanan` (
-  `id_pesanan` int(11) NOT NULL,
+  `id_pesanan` int(11) NOT NULL AUTO_INCREMENT,
   `waktu` datetime NOT NULL,
   `no_meja` int(11) NOT NULL,
   `status` enum('0','1') NOT NULL,
   PRIMARY KEY (`id_pesanan`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -121,9 +140,28 @@ CREATE TABLE `pesanan` (
 
 LOCK TABLES `pesanan` WRITE;
 /*!40000 ALTER TABLE `pesanan` DISABLE KEYS */;
-INSERT INTO `pesanan` VALUES (0,'2016-01-03 08:07:24',3,'0'),(1,'2016-01-03 05:14:38',1,'0'),(2,'2016-01-03 06:08:19',1,'0'),(3,'2016-01-03 08:00:38',2,'0');
+INSERT INTO `pesanan` VALUES (17,'2016-01-12 14:19:31',4,'1'),(19,'2016-01-12 14:58:33',2,'1');
 /*!40000 ALTER TABLE `pesanan` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 trigger after_insert_pesanan  AFTER INSERT on pesanan
+for each row
+begin
+	update meja set status = 0 where meja.no_meja = new.no_meja;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -134,4 +172,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-01-03 15:30:45
+-- Dump completed on 2016-01-12 20:54:58
